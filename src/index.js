e30f8b2c83248d00580e23becd2543f4794cbb9f
Irina Ostrometskaya <i.ostrometskaya@gmail.com>
2022-08-12 13:29:27 +0400

    feat: find solution for 17 tests from 20

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let openBrackets = [];
+  let pairBrackets = {};
+  for (let i = 0; i < bracketsConfig.length; i++){
+    let key = bracketsConfig[i][1];
+    let value = bracketsConfig[i][0];
+    openBrackets.push(bracketsConfig[i][0]);
+    pairBrackets[key] = value;
+  }
+  let stack = [];
+  for (let i = 0; i < str.length; i++) {
+    let currentSymbol = str[i];
+  
+  if (openBrackets.includes(currentSymbol)) {
+    stack.push(currentSymbol);
+  } else {
+    if (stack.length === 0) {
+      return false;
+    }
+  }
+    let topElement = stack[stack.length - 1];
+    
+    if (pairBrackets[currentSymbol] === topElement) {
+      stack.pop();
+    } 
+    }
+  
+  return stack.length === 0;
 }
